7133db106bae90addc1e0676bc484ebaa6761abd
AnushkaWijegoonawardana97 <anushkaduwolka123@gmail.com>
2023-08-06 23:53:17 +0530

    fixes

--- server.js
+++ server.js
@@ -11,8 +11,13 @@ app.use(express.json({ extended: false }));
 app.use("/api/posts", require("./routes/api/posts"));
 app.use("/api/favourite", require("./routes/api/favourite"));
 
-if(process.env.NODE_ENV === 'production') {
-    app.use()
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/next"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "next", "index.html"));
+  });
 }
 
 const PORT = process.env.PORT || 5000;
