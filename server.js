0bb6cfa16da82b5282a9dd8a24bdbd2a157cefa6
AnushkaWijegoonawardana97 <anushkaduwolka123@gmail.com>
2023-08-06 20:08:40 +0530

    fixes

--- server.js
+++ server.js
@@ -6,4 +6,4 @@ app.get("/", (request, response) => response.send("API is running..."));
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server Started on port ${PORT}...`));
+app.listen(PORT, () => console.log("Server Started on port "));
